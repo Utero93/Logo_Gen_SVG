5beb9359c380772e604224452e9327743b532533
Utero93 <nirvanautero93@gmail.com>
2023-12-16 16:24:48 -0500

    cleaning up and debugging

--- index.js
+++ index.js
@@ -1,66 +1,56 @@
-// Import the necessary packagesneeded for this application:
-
-// Import Inquirer
 const inquirer = require('inquirer');
-
-// Import File System(fs)
-const fs = require('fs').promises; // Using fs.promises to make file operations promise-based
-
-// Import shapes modules
+const fs = require('fs').promises; 
 const {Triangle, Circle, Square} = require('./lib/shapes');
 
-//--------------********-------------------*********--------*****-----
 
-// Inquirer questions saved in an array of objects:
 const questions = [
 
-// Enter text for the logo (Must not be more than 3 characters long).
+
   {
     type: 'input',
     name: 'text',
-    message: 'Enter text for the logo (Must not be more than 3 characters long).'
+    message: 'Enter text for the logo, it must be longer than 3 characters.'
   },
 
-// Enter a text color
   {
     type: 'input',
     name: 'textColor',
-    message: 'Enter a text color.'
+    message: 'Enter the color for your text.'
   },
 
-// Select a shape for the logo
   {
     type: 'list',
     name: 'shape',
-    message: 'Select a shape for your logo.',
+    message: 'Select a shape for your specific logo.',
     choices: ['triangle', 'circle', 'square']
   },
 
-// Enter a shape color
+
   {
     type: 'input',
     name: 'shapeColor',
-    message: 'Enter a shape color.'
+    message: 'Enter a color for the shape.'
   },
 ];
 
-//--------------********-------------------*********--------*****-----
 
-// Svg class w/constructor w/methods for setting text and shape elements and returning a working svg file
 class Svg {
 
   constructor(){
-    this.textEl = ''; // initializes text element
-    this.shapeEl = ''; // initializes shape element
+
+    this.textEl = ''; 
+    this.shapeEl = ''; 
+
   }
 
   render(){
+
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeEl}${this.textEl}</svg>`
+
   }
 
   setText(shape, text, textColor){
 
-    // checks shape and changes text y-coordinate so it fits better
 
     if(shape === "triangle"){
 
@@ -79,36 +69,37 @@ class Svg {
   }
 
   setShape(shape){
+
     this.shapeEl = shape.render();
+  
   }
 }
 
-//--------------********-------------------*********--------*****-----
 
-// Async function to initialize app
+
+
 async function init() {
+
   try {
 
-    // pass questions here
-    const data = await inquirer.prompt(questions);
+    const data = await prompt(questions);
 
-    // ensuring user only enters up to 3 characters of text
     let text = '';
 
     if (data.text.length > 0 && data.text.length < 4){
 
-      text = data.text; // valid character length
+      text = data.text;
 
-    } else { // invalid character length
+    } else {
 
       console.log('Invalid character length. Text for logo cannot be any more than 3 characters long.'); // error message
       
-      return; // end function
+      return;
     }
 
     let shapeElement;
 
-    // Create the appropriate shape element based on user selection
+
     switch (data.shape.toLowerCase()) {
       case 'triangle':
         shapeElement = new Triangle();
@@ -121,9 +112,9 @@ async function init() {
         break;
     }
     
-    shapeElement.setColor(data.shapeColor); // sets shapeColor based on user input
+    shapeElement.setColor(data.shapeColor); 
 
-    // console.log(shapeElement);
+
 
     const svgFile = new Svg ();
 
@@ -132,17 +123,15 @@ async function init() {
 
     const svgContent = svgFile.render();
 
-    // console.log(svgContent);
+
     
 
-    // Writes SVG file using fs.promises.writeFile
     await fs.writeFile('logo.svg', svgContent);
 
-    console.log('Successfully generated logo.svg file!');
+    console.log('Created a logo.svg file.');
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
-// Function call to initialize app
 init();
